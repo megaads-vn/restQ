87ce7d3e16ae685f22db0e860eef063bea68419b
Kien Trung Nguyen <kient.nguyen175@gmail.com>
2022-04-23 09:17:19 +0700

    optimize: open CORS

--- start/routes.js
+++ start/routes.js
@@ -1,7 +1,23 @@
 module.exports = function ($route, $logger) {
-    /** Register HTTP requests **/
+    /** Register HTTP requests **/    
     $route.get("/", "HomeController@welcome");
-    $route.any("/*", "MessageController@onRequest");
+    $route.any("/*", "MessageController@onRequest", {
+        before: function (io) {
+            io.header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Credentials", "true")
+                .header("Access-Control-Max-Age", 28800)
+                .header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, PATCH")
+                .header("Access-Control-Allow-Headers", "Content-Type, X-Auth-Token, Origin, Authorization, X-XSRF-TOKEN, Role-User, Token")
+        }
+    });
+    $route.options("/*", function (io) {
+        io.header("Access-Control-Allow-Origin", "*")
+            .header("Access-Control-Allow-Credentials", "true")
+            .header("Access-Control-Max-Age", 28800)
+            .header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, PATCH")
+            .header("Access-Control-Allow-Headers", "Content-Type, X-Auth-Token, Origin, Authorization, X-XSRF-TOKEN, Role-User, Token")
+            .echo("POST, GET, OPTIONS, PUT, DELETE, PATCH");
+    });
     /** Register socket.io requests **/
     /** Register filters **/
 };
