9de67989405667b37ad0c8cb9a20b2b17c6482ba
Phu Luong <phult.contact@gmail.com>
2024-05-29 16:13:14 +0700

    feature: monitor

--- controllers/monitor-controller.js
+++ controllers/monitor-controller.js
@@ -1,6 +1,51 @@
 module.exports = MonitorController;
+const moment = require('moment');
+const knex = require('knex');
 
 function MonitorController($event, $config, $queueServer) {
+    const dbConnection = knex($config.get("database"));
+    var summaryDays = 7;
+    const summaryDateLabels = [];
+    for (let i = 0; i < summaryDays; i++) {
+        const date = moment().subtract(i, 'days').format('YYYY-MM-DD');
+        summaryDateLabels.push(date);
+    }
+    this.index = async function (io) {        
+        var summaryData = [];
+        var consumers = $config.get("consumers.consumers");
+        for (let index = 0; index < consumers.length; index++) {
+            let consumerSummaryData = {
+                name: consumers[index].name,
+                labels: summaryDateLabels,
+                data: []
+            };
+            const consumer = consumers[index];
+            const waitingSummary = await summaryMessages(consumer.name, "WAITING", summaryDays);
+            const failedSummary = await summaryMessages(consumer.name, "FAILED", summaryDays);
+            let waitingSummaryByDate = {
+                name: "WAITING",
+                data: []
+            }
+            let failedSummaryByDate = {
+                name: "FAILED",
+                data: []
+            };
+            summaryDateLabels.forEach(date => {
+                const waiting = waitingSummary.find(row => row.date === date);
+                waitingSummaryByDate.data.push(waiting ? waiting.count : 0);
+                const failed = failedSummary.find(row => row.date === date);
+                failedSummaryByDate.data.push(failed ? failed.count : 0);
+            });
+            consumerSummaryData.data.push(waitingSummaryByDate);
+            consumerSummaryData.data.push(failedSummaryByDate);
+            summaryData.push(consumerSummaryData);
+        }
+        io.render("/monitor/index", {
+            "summaryData": summaryData,
+            "serverStatus": $queueServer.isRunning ? "Running" : "Stopped",
+            "authToken": $config.get("auth.token"),
+        });
+    }
     this.stop = function (io) {
         io.json({
             status: $queueServer.stop(),
@@ -12,5 +57,20 @@ function MonitorController($event, $config, $queueServer) {
             status: $queueServer.start(),
             is_running: $queueServer.isRunning
         });
+    }
+    async function summaryMessages(consumer = "", status = "WAITING", days = 7) {
+        try {
+            const rows = await dbConnection('message')
+                .select(dbConnection.raw("DATE_FORMAT(created_at_time, '%Y-%m-%d') as date, count(*) as count"))
+                .where('status', status)
+                .andWhere('last_consumer', consumer)
+                .andWhere('created_at_time', '>=', moment().subtract(days, 'days').format('YYYY-MM-DD 00:00:00'))
+                .groupByRaw('cast(created_at_time as date)');
+
+            return rows;
+        } catch (error) {
+            console.error(error);
+            return [];
+        }
     }
 }
--- start/routes.js
+++ start/routes.js
@@ -1,12 +1,34 @@
-module.exports = function ($route, $logger) {
-    /** Register HTTP requests **/    
+module.exports = function ($route, $logger, $config) {
+    var auth = function (io) {
+        const authToken = $config.get("auth.token");
+        console.log("auth.token", authToken);
+        console.log("io.inputs.token", io.inputs.token);
+        if (authToken != null &&
+            (io.inputs.token == null || io.inputs.token !== authToken)
+        ) {
+            io.json({
+                "status": "error",
+                "message": "Invalid token"
+            });
+            return false;
+        }
+    }
+
+    /** Register HTTP requests **/
     $route.get("/", "HomeController@welcome");
     $route.get("/robots.txt", "HomeController@robots");
     $route.get("/favicon.ico", "HomeController@welcome");
     $route.get("/message/(:code)", "MessageController@get");
     $route.delete("/message/done", "MessageController@removeDoneMessages");
-    $route.any("/monitor/start", "MonitorController@start");
-    $route.any("/monitor/stop", "MonitorController@stop");    
+    $route.get("/monitor", "MonitorController@index", {
+        before: auth
+    });
+    $route.post("/monitor/start", "MonitorController@start", {
+        before: auth
+    });
+    $route.post("/monitor/stop", "MonitorController@stop", {
+        before: auth
+    });
     $route.any("/*", "MessageController@onRequest", {
         before: function (io) {
             io.header("Access-Control-Allow-Origin", "*")
