f939e5f5e9fb394802c5e7ebdf15582bf44319f0
TuanPA <chutieuk7i@gmail.com>
2025-03-27 16:13:43 +0700

    optimize: disable migrate

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 global.__dir = __dirname;
 const DbMigration = require(__dir + "/db-migration");
 
-DbMigration.migrate().then(() => {
-    require(__dir + "/core/app/start").start();
-});
-
+// DbMigration.migrate().then(() => {
+//     require(__dir + "/core/app/start").start();
+// });
+require(__dir + "/core/app/start").start();
 
