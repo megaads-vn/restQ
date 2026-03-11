885680c855f480cc3ae5566115d696aeda833bdf
tuananhzippy <kieutuananh1995@gmail.com>
2026-03-11 22:53:04 +0700

    fix: log postback

--- message-queue/message-queue-server.js
+++ message-queue/message-queue-server.js
@@ -285,14 +285,18 @@ class MQServer {
                     postBackRequestData = responseData.message.data;
                 }
                 try {
-                    await axios({
+                    let postbackData = {
+                        request: postBackRequestData,
+                        result: responseData.response.data
+                    };
+
+                    console.log(`Postback request to: ${responseData.message.postback_url}`, postbackData);
+                    let postbackResponse = await axios({
                         method: 'POST',
                         url: responseData.message.postback_url,
-                        data: {
-                            request: postBackRequestData,
-                            result: responseData.response.data
-                        }
+                        data: postbackData
                     });
+                    console.log(`Postback response from: ${responseData.message.postback_url}`, postbackResponse.data);
                 } catch (error) {
                     self.$logger.warning('Postback::error: ' + responseData.message.code + " - " + error.message);
                 }
